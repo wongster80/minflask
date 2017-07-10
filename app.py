fba1294e0d241d17f214098e3db9d22534a1266d
Aidan Wong <aw110f@aidanwong-dev-03.np.ev1.yellowpages.com>
2017-07-10 22:04:22 +0000

    Adding client IP address

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import socket
 
-
+# Initialize Flask application
 app = Flask(__name__)
 
 
+# Default route, print user's IP
 @app.route("/")
 def index():
-    return "Hello from FLASK. My Hostname is: %s \n" % (socket.gethostname())
+    ip = request.remote_addr
+    return "Hello from FLASK. My Hostname is: %s \n My IP address is: %s \n" % (socket.gethostname(), ip)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
